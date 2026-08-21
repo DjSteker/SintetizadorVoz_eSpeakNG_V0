a0928ad449b8383618a37c5eac0a1cecfb5af710
DjSteker <37488993+DjSteker@users.noreply.github.com>
2026-08-22 00:08:58 +0200

    Update author and improve documentation in interfaz_grafica.hpp
    Updated author information and enhanced documentation for the graphical interface module.

--- src/interfaz_grafica.hpp
+++ src/interfaz_grafica.hpp
@@ -2,10 +2,19 @@
  * interfaz_grafica.hpp
  *
  *  Created on: 28 jul 2026
- *      Author: usuario001
+ *      Author: DjSteker
  */
 
-#pragma once
+/**
+ * @file interfaz_grafica.hpp
+ * @brief Declaraciones públicas de la interfaz gráfica (GTK).
+ *
+ * Este módulo construye y muestra la ventana principal de la aplicación y
+ * expone la función para conectar con la señal "activate" de GtkApplication.
+ *
+ * @author usuario001
+ * @date 28 jul 2026
+ */
 
 #ifndef INTERFAZ_GRAFICA_HPP_
 #define INTERFAZ_GRAFICA_HPP_
@@ -14,8 +23,17 @@
 
 namespace InterfazGrafica {
 
-// Construye y muestra la ventana principal. Pensada para conectarse
-// directamente a la señal "activate" de GtkApplication.
+/**
+ * @brief Construye y muestra la ventana principal.
+ *
+ * Diseñada para conectarse directamente a la señal "activate" de GtkApplication:
+ * @code
+ * g_signal_connect(app, "activate", G_CALLBACK(InterfazGrafica::activar), NULL);
+ * @endcode
+ *
+ * @param app Puntero a GtkApplication que activa la ventana.
+ * @param datos_usuario Puntero de datos de usuario (no usado, puede ser nullptr).
+ */
 void activar(GtkApplication *app, gpointer datos_usuario);
 
 } // namespace InterfazGrafica
