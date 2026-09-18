a3c41bd16985d7d05794f82a3627966c9f06d9ce
DjSteker <37488993+DjSteker@users.noreply.github.com>
2026-09-18 23:10:11 +0200

    Create interfaz_grafica.hpp for GTK interface
    This header file contains declarations for the graphical interface using GTK, including the main window construction and signal connection.

---
+++ interfaz_grafica.hpp
@@ -0,0 +1,43 @@
+/*
+ * interfaz_grafica.hpp
+ *
+ *  Created on: 28 jul 2026
+ *      Author: DjSteker
+ */
+
+/**
+ * @file interfaz_grafica.hpp
+ * @brief Declaraciones públicas de la interfaz gráfica (GTK).
+ *
+ * Este módulo construye y muestra la ventana principal de la aplicación y
+ * expone la función para conectar con la señal "activate" de GtkApplication.
+ *
+ * @author usuario001
+ * @date 28 jul 2026
+ */
+
+#pragma once
+
+#ifndef INTERFAZ_GRAFICA_HPP_
+#define INTERFAZ_GRAFICA_HPP_
+
+#include <gtk/gtk.h>
+
+namespace InterfazGrafica {
+
+/**
+ * @brief Construye y muestra la ventana principal.
+ *
+ * Diseñada para conectarse directamente a la señal "activate" de GtkApplication:
+ * @code
+ * g_signal_connect(app, "activate", G_CALLBACK(InterfazGrafica::activar), NULL);
+ * @endcode
+ *
+ * @param app Puntero a GtkApplication que activa la ventana.
+ * @param datos_usuario Puntero de datos de usuario (no usado, puede ser nullptr).
+ */
+void activar(GtkApplication *app, gpointer datos_usuario);
+
+} // namespace InterfazGrafica
+
+#endif /* INTERFAZ_GRAFICA_HPP_ */
